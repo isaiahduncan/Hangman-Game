05d6b726dfd28c89c7ac470b0b970a70611b71a5
isaiahduncan <isaiahduncan1212@yahoo.com>
2019-07-08 17:32:51 -0400

    added console drawings

--- src/Hangman.java
+++ src/Hangman.java
@@ -11,9 +11,89 @@ public class Hangman {
 	/**
 	 * @param args
 	 */
+	private static int wguesses = 0, nguesses = 6;//wrong guesses and num guesses
+	public static void consoleDraw() {
+		
+		switch (wguesses) {
+			case 0 :
+				System.out.println("      __    ");
+				System.out.println("     |  |   ");
+				System.out.println("     |      ");
+				System.out.println("     |      ");
+				System.out.println("     |      ");
+				System.out.println("  ___|___   ");
+				break;
+			case 1 : 
+				System.out.println("      __    ");
+				System.out.println("     |  |   ");
+				System.out.println("     |  o   ");
+				System.out.println("     |      ");
+				System.out.println("     |      ");
+				System.out.println("  ___|___   ");
+				break;
+			case 2 :
+				System.out.println("      __    ");
+				System.out.println("     |  |   ");
+				System.out.println("     |  o   ");
+				System.out.println("     |  |   ");
+				System.out.println("     |      ");
+				System.out.println("  ___|___   ");
+				break;
+			case 3 :
+				System.out.println("      __    ");
+				System.out.println("     |  |   ");
+				System.out.println("     |  o   ");
+				System.out.println("     | /|   ");
+				System.out.println("     |      ");
+				System.out.println("  ___|___   ");
+				break;
+			case 4 : 
+				System.out.println("      __    ");
+				System.out.println("     |  |   ");
+				System.out.println("     |  o   ");
+				System.out.println("     | /|\\  ");
+				System.out.println("     |      ");
+				System.out.println("  ___|___   ");
+				break;
+			case 5 : 
+				System.out.println("      __    ");
+				System.out.println("     |  |   ");
+				System.out.println("     |  o   ");
+				System.out.println("     | /|\\  ");
+				System.out.println("     | /    ");
+				System.out.println("  ___|___   ");
+				break;
+			case 6 :
+				System.out.println("      __    ");
+				System.out.println("     |  |   ");
+				System.out.println("     |  o   ");
+				System.out.println("     | /|\\  ");
+				System.out.println("     | / \\  ");
+				System.out.println("  ___|___   ");
+				break;
+		}
+
+	}
+	
+	public static String hideSecret(String secret) {
+		
+		
+		return secret;
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String [] wordsAndPhrases = {"I love Computer Science", "Isaiah is awesome", "Google", "Amazon", "Japan is Beautiful"};
+		System.out.println("Welcome to Hangman!");
+		
+		String secret = wordsAndPhrases[(int) (Math.random()*wordsAndPhrases.length)];
+		
+		consoleDraw();
+		System.out.println(hideSecret(secret));
+		
+		
+		
 	}
 
 }
